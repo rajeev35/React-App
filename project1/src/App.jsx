01059f435d002faad1b20699207da776a9b1fe6f
rajeev35 <rajeev2020it049@abesit.edu.in>
2023-08-13 00:40:13 +0530

    Initial commit

---
+++ project1/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Hero from "./components/Hero";
+
+import Navigation from "./components/Navigation";
+const App = () => {
+
+    return ( 
+    
+    <div>
+
+        <Navigation />
+        <Hero />
+      
+
+    </div>
+
+    );
+
+};
+
+export default App;
